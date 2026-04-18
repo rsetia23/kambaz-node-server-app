d54c394f57555ab6d49339ecc85f786990b157c2
rsetia23 <rahulsetia2005@gmail.com>
2026-04-18 12:52:39 -0400

    fixes

--- Kambaz/Users/schema.js
+++ Kambaz/Users/schema.js
@@ -11,7 +11,7 @@ const userSchema = new mongoose.Schema(
     dob: Date,
     role: {
       type: String,
-      enum: ["STUDENT", "FACULTY", "ADMIN", "USER"],
+      enum: ["STUDENT", "TA", "FACULTY", "ADMIN", "USER"],
       default: "USER",
     },
     loginId: String,
